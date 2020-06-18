39fdc9fd421850257f1695750c4cc8179f203e18
Mahmoud Ali <mahmoud.ali@lambda3.com.br>
2020-06-17 22:24:06 -0300

    Initial commit

---
+++ VirusTrackingApp/VirusTrackingApp/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+using Application = Xamarin.Forms.Application;
+
+namespace VirusTrackingApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            PropertyChanged += ContentPageBase_PropertyChanged;
+        }
+        
+        private void ContentPageBase_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "SafeAreaInsets")
+            {
+                var safeAreaInsets = On<iOS>().SafeAreaInsets();
+                Application.Current.Resources["SafeAreaInsets"] = safeAreaInsets;
+            }
+        }
+    }
+}
